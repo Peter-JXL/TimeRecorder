531ecc139244dd7d5a00661b74a42b068f8e68f2
peterjxl <peterjxl@qq.com>
2022-06-23 21:37:37 +0800

    常用函数测试

--- src/data/test.js
+++ src/data/test.js
@@ -34,9 +34,64 @@ function returnOneDayData(dateInput) {
     return data
 }
 
+function insertTime(oneTime){
+    knex('dataTable')
+    .insert(oneTime).then(()=>{
+        console.log('insert Success');
+    })
+    .catch((err)=>{
+        console.log(err);
+        throw err
+    }).finally(()=>{
+        knex.destroy()
+    })
+    
+}
+
+function insertLabel(label){
+    knex('labelTable')
+    .insert(label).then(()=>{
+        console.log('insert Label Table Success');
+    })
+    .catch((err)=>{
+        console.log(err);
+        throw err
+    }).finally(()=>{
+        knex.destroy()
+    })
+}
+
+function updateOneTime(oneTime){
+    console.log(oneTime);
+
+
+}
+
+function updateLabel(oneTime){
+    console.log(oneTime);
+
+}
+
+function statTime(beginDate, endDate, firstLabel='all', secondLabel='all', timeNote='all'){
+
+}
+
 
 //returnLabelTableData()
 //returnOneDayData()
 
-
-
+const oneTime = {
+    recordDate: '20220623',
+    beginTime: '2022-06-23 00:00',
+    endTime: '2022-06-23 7:50',
+    firstLabel: '睡眠',
+    secondLabel: '睡眠',
+    timeNote: '睡眠'
+}
+// insertTime(oneTime)
+const label = {
+    firstLabel: '睡眠',
+    secondLabel: '睡眠',
+    timeNote:'睡眠fuck'
+}
+insertLabel(label)
