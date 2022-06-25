0c31e6a0de3bcc47ec7474b9d6db1d8135684c66
peterjxl <peterjxl@qq.com>
2022-06-25 16:12:16 +0800

    测试momentjs

--- src/data/testMomentJS.js
+++ src/data/testMomentJS.js
@@ -1,12 +1,14 @@
 const moment = require( "moment")
-let t1 = moment('2022-06-25 00:00','YYYY-MM-DD HH:SS')
-let t2 = moment('2022-06-25 07:50','YYYY-MM-DD HH:SS')
-// let dura = t2.format('x') - t1.format('x')
-// let temTime = this.moment.duration(dura)
+let t1 = moment('2022-06-20 00:00','YYYY-MM-DD HH:mm')
+let t2 = moment('2022-06-20 07:50','YYYY-MM-DD HH:mm')
+console.log(t2.diff(t1, 'minutes', true))  //420.0083333333333
 
-// console.log(temTime.days())
-// console.log(temTime.hours())
-// console.log(temTime.minutes())
 
-console.log(t2.diff(t1, 'minute'))
-console.log(typeof (t2.diff(t1, 'minute')))
+let dura = t2.format('x') - t1.format('x')
+let timeSpan = moment.duration(dura)
+console.log(timeSpan.days())     //0
+console.log(timeSpan.hours())    //7
+console.log(timeSpan.minutes())  //0
+
+console.log(timeSpan.asMinutes()); //420.0083333333333
+
