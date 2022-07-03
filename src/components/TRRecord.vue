92f4a0aa17ae21b34d68548a3ddbc04e25c78c6b
peterjxl <peterjxl@qq.com>
2022-07-03 11:42:29 +0800

    表格样式居中

--- src/components/TRRecord.vue
+++ src/components/TRRecord.vue
@@ -1,13 +1,16 @@
 <template>
   <el-row :gutter="20">
     <el-col :span="14">
-      <el-table :data="tableData" stripe border height="528" max-height="2000">
+      <el-table :data="tableData" stripe border height="528" max-height="2000" 
+      :header-cell-style="{'text-align': 'center'}"
+      :cell-style="{'text-align':'center'}">
+      >
         <el-table-column prop="beginTime" label="开始时间" width="85" />
         <el-table-column prop="endTime" label="结束时间" width="85" />
         <el-table-column prop="firstLabel" label="一级标签" width="100" />
         <el-table-column prop="secondLabel" label="二级标签" width="100" />
-        <el-table-column prop="timeNote" label="备注" width="120" />
-        <el-table-column label="操作">
+        <el-table-column prop="timeNote" label="备注" width="200" />
+        <el-table-column label="操作" width="100" >
           <template #default="scope">
               <el-button size="small" @click="deleteOneTime(scope.row.ID)">删除</el-button>
           </template>
