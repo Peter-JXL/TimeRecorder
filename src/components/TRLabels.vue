2c0911a08d402bf68909a9d2deadbf32c47ecc1b
peterjxl <peterjxl@qq.com>
2022-07-05 22:41:52 +0800

    开始编写标签管理模块

---
+++ src/components/TRLabels.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import DbUtils from '@/data/DbUtils'
+
+export default {
+  name: 'TRLabels',
+  data() {
+    return {
+      allLabels:[]
+    }
+  },
+  methods: {
+    loadLabelsData(){
+      DbUtils.getAllLabel().then((rows)=>{
+        rows.forEach( row => {
+          this.allLabels.push(row)
+        })
+      })
+    }
+  },
+  mounted() {
+    this.loadLabelsData()
+  }
+}
+</script>
+
+<style>
+
+</style>
