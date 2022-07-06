50eac2bf3e979fb2f183341a6195fe67be6669b4
peterjxl <peterjxl@qq.com>
2022-07-06 21:07:39 +0800

    :sparkles: 插入一条标签功能

--- src/components/TRLabels.vue
+++ src/components/TRLabels.vue
@@ -1,21 +1,45 @@
 <template>
    <div id="TRLabelsMain">
-      <el-table 
-      id="recordTable"
-      :data="allLabels" stripe border height="560" max-height="2000" 
-      :header-cell-style="{'text-align': 'center'}"
-      :cell-style="{'text-align':'center'}"
-      :default-sort="{ prop: 'date', order: 'descending' }"
-      >
-        <el-table-column prop="firstLabel" label="一级标签" width="120" sortable/>
-        <el-table-column prop="secondLabel" label="二级标签" width="120" sortable/>
-        <el-table-column prop="timeNote" label="备注" width="500" />
-        <el-table-column label="操作" width="100" >
-          <template #default="scope">
-              <el-button size="small" @click="deleteOneLabel(scope.row.ID)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <div id="TRLabelsMainLeft">
+          <el-table 
+            id="recordTable"
+            :data="allLabels" stripe border height="560" max-height="2000" 
+            :header-cell-style="{'text-align': 'center'}"
+            :cell-style="{'text-align':'center'}"
+            :default-sort="{ prop: 'date', order: 'descending' }"
+            >
+              <el-table-column prop="firstLabel" label="一级标签" width="120" sortable/>
+              <el-table-column prop="secondLabel" label="二级标签" width="120" sortable/>
+              <el-table-column prop="timeNote" label="备注" width="500" />
+              <el-table-column label="操作" width="100" >
+                <template #default="scope">
+                    <el-button size="small" @click="deleteOneLabel(scope.row.ID)">删除</el-button>
+                </template>
+              </el-table-column>
+          </el-table>
+      </div>
+      
+      <div id="TRLabelsMainRight">
+        <el-form :model="form" label-width="100px">
+          <el-form-item label="一级标签：" >
+            <el-input ref="timeNote" v-model="firstLaebl" type="text" clearable  style="width:200px" ></el-input>
+          </el-form-item>
+
+
+          <el-form-item label="二级标签：" >
+            <el-input ref="timeNote" v-model="secondLabel" type="text" clearable  style="width:200px" ></el-input>
+          </el-form-item>
+
+          <el-form-item label="备注：" >
+            <el-input ref="timeNote" v-model="timeNote" type="text" clearable  style="width:200px" ></el-input>
+          </el-form-item>
+
+          <el-form-item >
+            <el-button type="primary" @click="addOneLabel">添加</el-button>
+          </el-form-item>
+
+        </el-form>
+      </div>
     </div>
 </template>
 
@@ -26,10 +50,24 @@ export default {
   name: 'TRLabels',
   data() {
     return {
+      firstLaebl:'',
+      secondLabel:'',
+      timeNote:'',
       allLabels:[]
+      
     }
   },
   methods: {
+    addOneLabel(){
+      let obj = {
+        firstLabel: this.firstLaebl, 
+        secondLabel: this.secondLabel,
+        timeNote: this.timeNote
+      }
+      DbUtils.insertLabel(obj).then(()=>{
+        this.loadLabelsData()
+      })
+    },
     loadLabelsData(){
       this.allLabels = this.allLabels.splice(0, 0)
 
@@ -59,5 +97,15 @@ export default {
     width: 1200px;
     height: 580px;
     border: 1px green;
+  }
+
+  #TRLabelsMainLeft{
+    width: 900px;
+    height: 580px;
+  }
+
+  #TRLabelsMainRight{
+    width: 500px;
+    height: 580px;
   }
 </style>
