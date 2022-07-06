6284645bd143f41695292503fcf71b4403800628
peterjxl <peterjxl@qq.com>
2022-07-06 20:04:08 +0800

    添加排序功能

--- src/components/TRRecord.vue
+++ src/components/TRRecord.vue
@@ -7,12 +7,13 @@
       id="recordTable"
       :data="tableData" stripe border height="560" max-height="2000" 
       :header-cell-style="{'text-align': 'center'}"
-      :cell-style="{'text-align':'center'}">
+      :cell-style="{'text-align':'center'}"
+      :default-sort="{ prop: 'date', order: 'descending' }"
       >
-        <el-table-column prop="beginTime" label="开始时间" width="85" />
-        <el-table-column prop="endTime" label="结束时间" width="85" />
-        <el-table-column prop="firstLabel" label="一级标签" width="100" />
-        <el-table-column prop="secondLabel" label="二级标签" width="100" />
+        <el-table-column prop="beginTime" label="开始时间" width="110" sortable />
+        <el-table-column prop="endTime" label="结束时间" width="110" sortable/>
+        <el-table-column prop="firstLabel" label="一级标签" width="110" sortable />
+        <el-table-column prop="secondLabel" label="二级标签" width="110" sortable />
         <el-table-column prop="timeNote" label="备注" width="150" />
         <el-table-column label="操作" width="100" >
           <template #default="scope">
@@ -252,7 +253,7 @@ export default {
 }
 
 #TRRecordLeft{
-  width: 650px;
+  width: 750px;
   height: 500px;
 }
 
