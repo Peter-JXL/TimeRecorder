896f32d230e8cfdb923038850f4e0186fd3d9699
peterjxl <peterjxl@qq.com>
2022-07-06 20:16:11 +0800

    表格排序功能

--- src/components/TRLabels.vue
+++ src/components/TRLabels.vue
@@ -2,16 +2,17 @@
    <div id="TRLabelsMain">
       <el-table 
       id="recordTable"
-      :data="tableData" stripe border height="560" max-height="2000" 
+      :data="allLabels" stripe border height="560" max-height="2000" 
       :header-cell-style="{'text-align': 'center'}"
-      :cell-style="{'text-align':'center'}">
+      :cell-style="{'text-align':'center'}"
+      :default-sort="{ prop: 'date', order: 'descending' }"
       >
-        <el-table-column prop="firstLabel" label="一级标签" width="100" />
-        <el-table-column prop="secondLabel" label="二级标签" width="100" />
+        <el-table-column prop="firstLabel" label="一级标签" width="120" sortable/>
+        <el-table-column prop="secondLabel" label="二级标签" width="120" sortable/>
         <el-table-column prop="timeNote" label="备注" width="500" />
         <el-table-column label="操作" width="100" >
           <template #default="scope">
-              <el-button size="small" @click="deleteOneTime(scope.row.ID)">删除</el-button>
+              <el-button size="small" @click="deleteOneLabel(scope.row.ID)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -30,10 +31,17 @@ export default {
   },
   methods: {
     loadLabelsData(){
+      this.allLabels = this.allLabels.splice(0, 0)
+
       DbUtils.getAllLabel().then((rows)=>{
         rows.forEach( row => {
           this.allLabels.push(row)
         })
+      })
+    },
+    deleteOneLabel(ID){
+      DbUtils.deleteOneLabel(ID).then(()=>{
+        this.loadLabelsData()
       })
     }
   },
