f6884f24c5f6cb5c2df58e7218a52d7e56ab2803
peterjxl <peterjxl@qq.com>
2022-07-06 08:14:04 +0800

    引入table组件

--- src/components/TRLabels.vue
+++ src/components/TRLabels.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+   <div id="TRLabelsMain">
+      <el-table 
+      id="recordTable"
+      :data="tableData" stripe border height="560" max-height="2000" 
+      :header-cell-style="{'text-align': 'center'}"
+      :cell-style="{'text-align':'center'}">
+      >
+        <el-table-column prop="firstLabel" label="一级标签" width="100" />
+        <el-table-column prop="secondLabel" label="二级标签" width="100" />
+        <el-table-column prop="timeNote" label="备注" width="500" />
+        <el-table-column label="操作" width="100" >
+          <template #default="scope">
+              <el-button size="small" @click="deleteOneTime(scope.row.ID)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
 </template>
 
 <script>
@@ -27,6 +43,13 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  #TRLabelsMain{
+    display: flex;
+    justify-content: flex-start;
+    align-items: space-between;
+    width: 1200px;
+    height: 580px;
+    border: 1px green;
+  }
 </style>
