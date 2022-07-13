7f9bbf1968bc5fe1a79186492296e121174a3819
peterjxl <peterjxl@qq.com>
2022-07-13 22:01:42 +0800

    添加注释

--- src/components/TRTabs.vue
+++ src/components/TRTabs.vue
@@ -1,23 +1,22 @@
 <template>
   <el-tabs class="demo-tabs" v-model="activeName">
     <el-tab-pane label="记录时间" name="first">
-      <TRRecord></TRRecord>
+      <TRRecord/>
     </el-tab-pane>
 
 
     <el-tab-pane label="当日时间分析" name="second">
-      <TRDayAnalyzeVue></TRDayAnalyzeVue>
+      <TRDayAnalyzeVue/>
     </el-tab-pane>
 
     
     <el-tab-pane label="标签管理" name="third">
-      <TRLabels></TRLabels>
+      <TRLabels/>
     </el-tab-pane>
 
     
     <el-tab-pane label="数据统计" name="fourth">
-    
-
+      <TRDaysAnalyze/>
     </el-tab-pane>
   </el-tabs>
 </template>
@@ -27,18 +26,20 @@ import mousetrap from 'mousetrap'
 import TRRecord from './TRRecord.vue'
 import TRDayAnalyzeVue from './TRDayAnalyze.vue'
 import TRLabels from './TRLabels.vue'
+import TRDaysAnalyze from './TRDaysAnalyze.vue'
+
 export default {
     name: "TRTabs",
-    components: { TRRecord, TRDayAnalyzeVue, TRLabels },
+    components: { TRRecord, TRDayAnalyzeVue, TRLabels, TRDaysAnalyze },
     data() {
       return {
         activeNameArr: ['first',"second","third","fourth"],  //当前标签页的name集合
         activeIndex: 0,  //当前激活的标签页下标
-        activeName: 'first'  //绑定Tabs组件的激活标签
+        activeName: 'fourth'  //绑定Tabs组件的激活标签
       }
     },
     watch:{
-      //当activeIndex变化时，切换Tabs组件的标签页
+      //当activeIndex变化时，切换为对应的Tabs组件的标签页
       activeIndex(){
         this.activeName = this.activeNameArr[this.activeIndex]
       }
