57b648247910d2f11fa54939f8617e59ad2eed54
peterjxl <peterjxl@qq.com>
2022-07-14 21:19:36 +0800

    统计时间模块初步完成

--- src/components/TRDaysAnalyze.vue
+++ src/components/TRDaysAnalyze.vue
@@ -2,9 +2,7 @@
   <div id="TRDaysAnalyzeBox">
 
 
-    <div id="chinaChartDays">
-      
-    </div>
+    <div id="chinaChartDays"></div>
 
 
     <div id="chinaChartOptions">
@@ -14,7 +12,7 @@
         </el-form-item>
 
          <el-form-item label="结束日期：">
-          <input type="date" v-model="beginDate"  class="myInput inputDate" >
+          <input type="date" v-model="endDate"  class="myInput inputDate" >
         </el-form-item>
 
         <el-form-item label="图表类型：">
@@ -40,7 +38,7 @@
 
 <script>
 import DbUtils from '@/data/DbUtils'
-import moment from 'moment'
+import moment from "moment";
 var myChart;
 
 
@@ -48,10 +46,47 @@ export default {
   name: 'TRDaysAnalyze',
   data() {
     return {
-      beginDate: '2022-07-13',
-      endDate: '2022-07-13',
+      beginDate: '',
+      endDate: '',
       chartType: 'pie',
       showToolTip: true,
+      charOption: {
+        title: {
+          text: "当日数据统计",
+        },
+        tooltip: {
+          show: true,
+          trigger: "item",
+          formatter: '{b} {c}分钟  占比{d}%'
+        },
+        legend: {
+          top: "5%",
+          left: "center",
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        series: [
+          {
+            name: "当日数据统计",
+            type: "pie",
+            radius: "50%",
+            label:{
+              formatter: '{b}: {c}分钟 '
+            },
+            data: [],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      },
     }
   },
   watch:{
@@ -68,8 +103,48 @@ export default {
     }
   },
   methods: {
-    statistics(){
+    //统计开始日期（含）到结束日期（含）之间的数据，每个一级标签花了多少时间
+    statistics(){    
+      let beginDate =  moment(this.beginDate, "YYYY-MM-DD");
+      let endDate =  moment(this.endDate, "YYYY-MM-DD").add(1,'d');
+      let days = endDate.diff(beginDate, 'days')
 
+      let daysMinutes = days * 24 * 60; //总共多少分钟
+      let recordDateMinutes = 0  //统计用户记录的分钟数
+      let labelTimeMap = new Map();
+      this.charOption.series[0].data = this.charOption.series[0].data.slice(0,0)  //清空原本的数据
+
+      DbUtils.statDasyTime(beginDate.format("YYYY-MM-DD"), endDate.format("YYYY-MM-DD")).then( rows =>{
+        rows.forEach((row) => {
+          var firstLabel = row["firstLabel"];
+          var beginTime = moment(row["beginTime"], "YYYY-MM-DD HH:mm");
+          var endTime = moment(row["endTime"], "YYYY-MM-DD HH:mm");
+          var timeSpan = endTime.diff(beginTime, "minute");
+
+          if (labelTimeMap.has(firstLabel)) {
+            labelTimeMap.set(firstLabel,labelTimeMap.get(firstLabel) + timeSpan);
+          } else {
+            labelTimeMap.set(firstLabel, timeSpan, labelTimeMap);
+          }
+          recordDateMinutes += timeSpan;
+        });
+
+        //不相等则说明用户有未记录的时间，或者多记录的时间
+        if (recordDateMinutes !== daysMinutes) {
+          let errLabel = recordDateMinutes < daysMinutes ? "未记录的时间" : "多记录的时间"; //检查用户是少记录了时间还是多记录了时间
+          labelTimeMap.set(errLabel, Math.abs(recordDateMinutes - daysMinutes));
+        }
+
+        //将
+        labelTimeMap.forEach((value,key ) => {
+          this.charOption.series[0].data.unshift({
+            value: value,
+            name: key,
+          });
+        });
+        
+        myChart.setOption(this.charOption);
+      })
     },
     loadPie(){
 
@@ -85,7 +160,9 @@ export default {
     },
   },
   mounted() {
-    
+    myChart = this.$echarts.init(document.getElementById("chinaChartDays"));
+    this.beginDate = this.endDate = moment().format('YYYY-MM-DD')
+    this.statistics()
   },
 
 }
